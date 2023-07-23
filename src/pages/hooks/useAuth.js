bbb22c3bba774005c7a37205a0836ac133763d79
Sajeevan R S <84395078+Sajeevan-R-S@users.noreply.github.com>
2023-07-23 19:41:30 +0530

    Delete src/pages/hooks directory

--- src/pages/hooks/useAuth.js
+++
@@ -1,9 +0,0 @@
-// This is just to replace importing useContext and AuthContext everytime.
-import { useContext } from "react";
-import AuthContext from "../context/AuthProvider";
-
-const useAuth = () => {
-  return useContext(AuthContext);
-};
-
-export default useAuth;
